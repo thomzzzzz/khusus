4fbdee8725d2eaa9d236a543fef35dd6f461de84
thomzzzzz <159254385+thomzzzzz@users.noreply.github.com>
2025-04-24 03:06:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// Anda bisa menambahkan interaksi JavaScript di sini,
+// misalnya untuk menambahkan produk ke keranjang, memvalidasi formulir, dll.
+// Untuk contoh sederhana ini, kita biarkan kosong.
